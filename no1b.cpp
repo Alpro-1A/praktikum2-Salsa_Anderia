3a7eeb9096ec2db983441e0b63fc0e0613d4ff7a
salsaanderia <2310631170@student.unsika.ac.id>
2023-10-20 10:49:39 +0700

    mengumpulkan tugas no 1b

---
+++ no1b.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int firstValue, secondValue, variabelBaru;
+    firstValue = 10;
+    secondValue = 8;
+
+    cout<< "Menukar nilai Variabel tanpa variabel tambahan"<<endl;
+    cout<< "first value: " <<firstValue<<endl;
+    cout<< "second value: "<< secondValue<<endl;
+    cout<< "----------------------------------------------"<<endl;
+ 	cout<< "first value: " <<secondValue<<endl;
+  	cout<< "second value: "<<firstValue<< endl;
+
+    return 0;
+}
